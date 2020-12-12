80ce30e5388067d9ea3413b58b8a54190c420fdd
omegacat <anisimov.develop@gmail.com>
2020-12-13 02:00:34 +0300

    Add controller method for checking grants for file in IPFS;

--- src/main/java/ethereum/web/CrosschainReceiverEthereumController.java
+++ src/main/java/ethereum/web/CrosschainReceiverEthereumController.java
@@ -1,17 +1,20 @@
 package ethereum.web;
 
+import ethereum.web.assemblers.ChangeFileOwnerEventResourceAssembler;
+import ethereum.web.assemblers.InitFileStoreEventResourceAssembler;
+import ethereum.web.dto.ChangeFileOwnerEventResource;
+import ethereum.web.dto.InitFileStoreEventResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import ethereum.services.EthereumService;
 import ipfs.services.IPFSService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static ethereum.web.GeneralEthereumController.FILE_STORAGE_CONTRACT_ADDRESS;
 
@@ -22,8 +25,37 @@ public class CrosschainReceiverEthereumController {
     private EthereumService ethereumService;
     @Autowired
     private IPFSService ipfsService;
+    @Autowired
+    private InitFileStoreEventResourceAssembler initFileStoreEventResourceAssembler;
+    @Autowired
+    private ChangeFileOwnerEventResourceAssembler changeFileOwnerEventResourceAssembler;
 
-    private String CROSS_NODE_PRIVATE_KEY="0x5c690e5e749aaca8cdd21dfffd1cda77d1dca1326f0d9c756f5943947f8f5516";
+    private String CROSS_NODE_PRIVATE_KEY = "0x5c690e5e749aaca8cdd21dfffd1cda77d1dca1326f0d9c756f5943947f8f5516";
+
+    @GetMapping("/checkGrantForFile")
+    public boolean checkGrantForFile(
+            @RequestParam
+                    String ipfsHashOfFile,
+            @RequestParam
+                    String senderPublicKey // sender of file
+    ) {
+        List<InitFileStoreEventResource> listOfAllInits =
+                ethereumService.getInitFileStoreEvents(FILE_STORAGE_CONTRACT_ADDRESS, CROSS_NODE_PRIVATE_KEY).stream()
+                        .map(initFileStoreEventResourceAssembler::toResource)
+                        .collect(Collectors.toList());
+        List<ChangeFileOwnerEventResource> listOfAllChangeOwners =
+                ethereumService.getChangeFileOwnerEvents(FILE_STORAGE_CONTRACT_ADDRESS, CROSS_NODE_PRIVATE_KEY).stream()
+                        .map(changeFileOwnerEventResourceAssembler::toResource)
+                        .collect(Collectors.toList());
+        boolean isInitBySender = listOfAllInits.stream()
+                .anyMatch(res -> res.getHashOfFile().equals(ipfsHashOfFile) && res.getOwner().equals(senderPublicKey));
+        if (!isInitBySender) {
+            return listOfAllChangeOwners.stream().anyMatch(res -> res.getActualOwner().equals(senderPublicKey));
+        } else {
+            return listOfAllChangeOwners.stream().anyMatch(res -> !res.getPrevOwner().equals(senderPublicKey));
+        }
+    }
+
 
     //    UNUSED
     @PostMapping("/init")
